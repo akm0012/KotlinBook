b2de4e7400cf35a516c037d4bf9bf2face0165db
akm0012 <akm0012@auburn.edu>
2018-08-15 16:50:07 -0400

    Added the solution to the challenege.

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -5,7 +5,24 @@ fun main(args: Array<String>) {
     val isImmortal = false
 
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
-    val auraColor = if (auraVisible) "GREEN" else "NONE"
+//    val auraColor = if (auraVisible) "GREEN" else "NONE"
+
+    // I don't think this equation is right, have not seen a Karma below 11 yet
+    val karma = (Math.pow(Math.random(), (110 - healthPoints) / 100.0) * 20).toInt()
+
+    println("You have $karma karma...")
+
+    val auraColor = when (karma) {
+        in 0..5 -> "Red"
+        in 6..10 -> "Orange"
+        in 11..15 -> "Purple"
+        in 16..20 -> "Green"
+        else -> "Unknown"
+    }
+
+    if (auraVisible) {
+        println("Your aura is a brilliant $auraColor glow!")
+    }
 
     println("Aura: $auraColor " +
         "(Blessed: ${if (isBlessed) "YES" else "NO"})")
