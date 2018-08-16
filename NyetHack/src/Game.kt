972328f5a56c6e5ffbfada2a4f4159da01644f74
akm0012 <akm0012@auburn.edu>
2018-08-16 09:34:00 -0400

    Finished Chapter 4 - Functions

--- NyetHack/src/Game.kt
+++ NyetHack/src/Game.kt
@@ -1,16 +1,36 @@
 fun main(args: Array<String>) {
     val name = "Madrigal"
-    var healthPoints = 95
+    var healthPoints = 89
     var isBlessed = true
     val isImmortal = false
 
+    // Aura Color
+    val auraColor = auraColor(isBlessed, healthPoints, isImmortal)
+
+    val healthStatus = formatHealthStatus(healthPoints, isBlessed)
+
+    // Player status
+    printPlayerStatus(auraColor, isBlessed, name, healthStatus)
+
+    // Naming the arguments lets you put them in any order.
+    printPlayerStatus(name = "Andrew", auraColor = "Red", healthStatus = "Doing Great!", isBlessed = false)
+
+    castFireball()
+}
+
+private fun printPlayerStatus(auraColor: String, isBlessed: Boolean, name: String, healthStatus: String) {
+    println("Aura: $auraColor " +
+            "(Blessed: ${if (isBlessed) "YES" else "NO"})")
+    println("$name $healthStatus")
+}
+
+private fun auraColor(isBlessed: Boolean, healthPoints: Int, isImmortal: Boolean): String {
     val auraVisible = isBlessed && healthPoints > 50 || isImmortal
     val auraColor = if (auraVisible) "GREEN" else "NONE"
+    return auraColor
+}
 
-    println("Aura: $auraColor " +
-        "(Blessed: ${if (isBlessed) "YES" else "NO"})")
-
-    val healthStatus = when (healthPoints) {
+private fun formatHealthStatus(healthPoints: Int, isBlessed: Boolean): String = when (healthPoints) {
         100 -> "is in excellent condition"
         in 90..99 -> "has a few scratches"
         in 75..89 -> {
@@ -24,6 +44,5 @@ fun main(args: Array<String>) {
         else -> "is in awful condition!"
     }
 
-    // Player status
-    println("$name $healthStatus")
-}
+private fun castFireball(numFireballs: Int = 2) =
+        println("A glass of Fireball springs into existence (x$numFireballs)")
